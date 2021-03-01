193fe9a1e167a3f12fbe23f5ec7f383d6fb45bed
Muhammad Ibrahim <muhammadibrahim@Muhammads-MacBook-Air.local>
2021-02-28 21:05:50 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import PageWrapper from './components/PageWrapper';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Pages
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <PageWrapper>
+          <Route 
+            exact="true"
+            path="/"
+            component={Home} 
+          />
+
+          <Route 
+            exact="true"
+            path="/about"
+            component={About}
+          />
+        </PageWrapper>
+      </Router>
+    );
+  }
 }
+
 
 export default App;
