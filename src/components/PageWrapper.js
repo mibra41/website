1b6d832740e40ee421d379e0dd3751796f716cf1
Muhammad Ibrahim <muhammadibrahim@Muhammads-MacBook-Air.local>
2021-03-15 10:23:35 -0500

    resume sections made, styling needs to be done

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -3,28 +3,36 @@ import {Link} from 'react-router-dom';
 import PageHeader from './common/PageHeader';
 
 class PageWrapper extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: "",
+            paddingTop: "15rem"
+        };
+    }
     render() {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark fixed-top" style={{backgroundColor: "#0009"}} id="mainNav">
                     <div className="container">
-                    <Link className="navbar-brand js-scroll-trigger" to="/">Home</Link>
+                    <Link className="navbar-brand js-scroll-trigger" to="/" onClick={() => this.setState({value: "", paddingTop: "15rem"})}>Home</Link>
                     <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         Menu
                     <i className="fas fa-bars ml-1"></i>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul className="navbar-nav text-uppercase ml-auto">
-                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/resume">Resume</Link></li>
-                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/resources">Resources</Link></li>
-                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/blog">Thoughts</Link></li>
-                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/memories">Memories</Link></li>
-                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/about">About</Link></li>
+                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/resume" onClick={() => this.setState({value: "resume", paddingTop: ""})}>Resume</Link></li>
+                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/resources" onClick={() => this.setState({value: "useful resources", paddingTop: ""})}>Resources</Link></li>
+                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/blog" onClick={() => this.setState({value: "random thoughts", paddingTop: ""})}>Thoughts</Link></li>
+                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/memories" onClick={() => this.setState({value: "memories", paddingTop: ""})}>Memories</Link></li>
+                            <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/about" onClick={() => this.setState({value: "about me", paddingTop: ""})}>About</Link></li>
                         </ul>
                     </div>
                     </div>
                 </nav>
-                <PageHeader></PageHeader>
+                <PageHeader value={this.state.value} paddingTop={this.state.paddingTop}></PageHeader>
                 {this.props.children}
             </div>
         );
--- src/components/common/PageHeader.js
+++ src/components/common/PageHeader.js
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 class PageHeader extends Component {
     render(){
         return(
-            <header className="masthead-small">
+            <header className="masthead-small" style={{paddingTop: this.props.paddingTop}}>
                     <div className="container">
-                        <div className="masthead-heading text-uppercase">Resume</div>
+                        <div className="masthead-heading text-uppercase">{this.props.value}</div>
                     </div>
             </header>
         );
